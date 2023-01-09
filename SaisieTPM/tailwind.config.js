9732eb65523678c00c9d82a4579cb4bb14de28b6
Satmyx <dylanesculier@gmail.com>
2023-01-09 16:07:02 +0100

    Fresh Start
    :+1: : Ajout de TailwindCSS
    :+1: : Ajout de TailwindElements
    :+1: : Navbar
    :+1: : Webpack Encore
    :runner: : Bundle security en configuration

---
+++ SaisieTPM/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./templates/**/*.html.twig', './node_modules/tw-elements/dist/js/**/*.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('tw-elements/dist/plugin')],
+}
