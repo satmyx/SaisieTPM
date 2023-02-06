827c48715b59e60259c562bb486292c66f7fd3c9
Satmyx <dylanesculier@gmail.com>
2023-02-06 16:42:36 +0100

    Fix + error
    :+1: : Ajout des erreurs 500, 404.
    :+1: : Ajout du package TWIG Bridge.
    :wrench: : Fix du package JS pas charger TW ELEMENTS.
    
    :warning: : Passage en prod !!!

--- SaisieTPM/tailwind.config.js
+++ SaisieTPM/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./templates/**/*.html.twig', './node_modules/tw-elements/dist/js/**/*.js'],
+  content: ['./templates/**/*.html.twig'],
   theme: {
     extend: {},
   },
